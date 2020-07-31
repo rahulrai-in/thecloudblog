d8054493b3b971903495b27c8c10471214fe1110
Rahul Rai <rahulrai@live.com>
2020-08-01 00:58:01 +1000

    test 23

--- tcb/functions/oggen/oggen.js
+++ tcb/functions/oggen/oggen.js
@@ -1,11 +1,13 @@
-// const puppeteer = require("puppeteer-core");
+const puppeteer = require("puppeteer-serverless");
 // const chromium = require("chrome-aws-lambda");
-// const renderSocialImage = require("puppeteer-social-image");
+const renderSocialImage = require("puppeteer-social-image");
 
-import puppeteer from "puppeteer-serverless";
-import renderSocialImage from "puppeteer-social-image";
+// import puppeteer from "puppeteer-serverless";
+// import renderSocialImage from "puppeteer-social-image";
+let browser;
 
 exports.handler = async function (event, context, callback) {
+  browser = browser || (await puppeteer.launch({}));
   await renderSocialImage({
     template: "basic",
     templateParams: {
@@ -13,13 +15,6 @@ exports.handler = async function (event, context, callback) {
         "https://images.unsplash.com/photo-1557958114-3d2440207108?w=1950&q=80",
       title: "Hello, world",
     },
-
-    browser: await puppeteer.launch({}),
-    // browser: await puppeteer.launch({
-    //   executablePath: await chromium.executablePath,
-    //   // args: chromium.args,
-    //   // defaultViewport: chromium.defaultViewport,
-    //   // headless: chromium.headless,
-    // }),
+    browser,
   });
 };
