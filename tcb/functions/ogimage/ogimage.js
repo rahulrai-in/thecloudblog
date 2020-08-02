994fcf0a95b5dce8a75991f02dfa975391b323d6
Rahul Rai <rahulrai@live.com>
2020-08-02 15:43:18 +1000

    style go

--- tcb/functions/ogimage/ogimage.js
+++ tcb/functions/ogimage/ogimage.js
@@ -42,19 +42,18 @@ async function getScreenshot(url, isDev) {
   return base64Image;
 }
 
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   const qs = new URLSearchParams(event.queryStringParameters);
   console.log(
     `${
-      process.env.URL || `http://localhost:8888`
-    }/thumbnail.html?${qs.toString()}`
+      process.env.URL || `http://localhost:1313`
+    }/opengraph.html?${qs.toString()}`
   );
   const photoBuffer = await getScreenshot(
     `${
-      process.env.URL || `http://localhost:8888`
-    }/thumbnail.html?${qs.toString()}`,
-    // Here we need to pass a boolean to say if we are on the server. Netlify has a bug where process.env.NETLIFY is undefiend in functions so I'm using one of the only vars I can find
+      process.env.URL || `http://localhost:1313`
+    }/opengraph.html?${qs.toString()}`,
+    // Here we need to pass a boolean to say if we are on the server. Netlify has a bug where process.env.NETLIFY is undefined in functions so I'm using one of the only vars I can find
     // !process.env.NETLIFY
     process.env.URL.includes("http://localhost")
   );
